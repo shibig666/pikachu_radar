15c6212100d576b9f4d774194a234adca0e30003
shibig666 <shiyxsbig@outlook.com>
2024-12-17 11:51:50 +0800

    change

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import radar
 import cv2
 
-img=cv2.imread('interface/1.jpg')
+img = cv2.imread('interface/1.jpg')
 detector=radar.Detector('weights/car.pt','weights/armor.pt','interface/map.png')
-detector.detect(img)
-detector.display()
-img=detector.plot_cars(img)
-cv2.imshow('result',img)
-cv2.waitKey(0)
+detector.map.select_src_point(img)
+detector.map.select_dst_point()
+detector.map.show_map()
+# detector.detect(img)
+# detector.display()
+# img=detector.plot_cars(img)
+# cv2.imshow('result',img)
+# cv2.waitKey(0)
--- radar/types.py
+++ radar/types.py
@@ -34,11 +34,12 @@ class Armor:
 # 机器人类
 class Car:
     def __init__(self, box, image):
-        self.armors = []    # 内含装甲板
+        self.armors = []  # 内含装甲板
         self.image = image  # ROI图像
         self.box = box  # YOLO识别的机器人框位置
         self.id = "-1"  # 机器人ID
-        self.type = "unknown"   # 机器人类型
+        self.type = "unknown"  # 机器人类型
+        self.center = ((box[0] + box[2]) // 2, (box[1] + box[3]) // 2)
 
     # 添加装甲板
     def add_armor(self, armor):
@@ -79,15 +80,64 @@ class Car:
         else:
             color = (0, 255, 0)
         cv2.rectangle(image, (self.box[0], self.box[1]), (self.box[2], self.box[3]), color, 2)
-        cv2.putText(image, f"ID:{self.id}", (self.box[0], self.box[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv2.putText(image, f"ID:{self.id}", (self.box[0], self.box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv2.circle(image, self.center, 5, color, -1)
         for armor in self.armors:
-            cv2.rectangle(image, (self.box[0]+armor.box[0], self.box[1]+armor.box[1]),
-                          (self.box[0]+armor.box[2], self.box[1]+armor.box[3]), color, 2)
+            cv2.rectangle(image, (self.box[0] + armor.box[0], self.box[1] + armor.box[1]),
+                          (self.box[0] + armor.box[2], self.box[1] + armor.box[3]), color, 2)
         return image
 
 
 class Map:
     def __init__(self, image_path):
+        self.src_image = None
         self.image = cv2.imread(image_path)
         self.shape = self.image.shape
         self.cars = []
+        self.src_points = []
+        self.dst_points = []
+
+    def _resize_image(self, image, scale):
+        return cv2.resize(image, (0, 0), fx=scale, fy=scale)
+
+    def _mouse_callback(self, event, x, y, flags, param):
+        points, scale, image = param
+        if event == cv2.EVENT_LBUTTONDOWN:
+            points.append((int(x // scale), int(y // scale)))
+            cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
+
+    def _select_points(self, image, points, scale, window_name="Select Points"):
+        points.clear()
+        resized_image = self._resize_image(image, scale)
+        cv2.imshow(window_name, resized_image)
+        param = (points, scale, resized_image)
+        cv2.setMouseCallback(window_name, self._mouse_callback, param=param)
+
+        while True:
+            cv2.imshow(window_name, resized_image)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                if len(points) < 4:
+                    print("Please select 4 points")
+                    continue
+                break
+
+        print(f"Selected points: {points}")
+        cv2.destroyAllWindows()
+
+    def select_src_point(self, src_image, scale=0.5):
+        self._select_points(src_image, self.src_points, scale, window_name="Select Source Points")
+
+    def select_dst_point(self, scale=0.5):
+        self._select_points(self.image, self.dst_points, scale, window_name="Select Destination Points")
+
+    def show_map(self):
+        """显示地图"""
+        image=self.image.copy()
+        for dst_point in self.dst_points:
+            cv2.circle(image, dst_point, 5, (0, 0, 255), -1)
+        cv2.imshow("Map", cv2.resize(image, (0, 0), fx=0.5, fy=0.5))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
